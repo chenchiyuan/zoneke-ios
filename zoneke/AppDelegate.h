6ce21d5d88e94db8398cab6dcf9bcd547fea4a0a
chenchiyuan <chenchiyuan03@gmail.com>
2012-04-22 12:44:53 +0800

    Initial Commit

---
+++ zoneke/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  zoneke
+//
+//  Created by tukeQ tukeQ on 12-4-22.
+//  Copyright (c) 2012年 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
